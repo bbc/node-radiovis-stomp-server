de1b7ca5306ab11c3190f9473f1df048df76ab97
Nicholas Humfrey <njh@aelius.com>
2017-03-25 23:32:01 +0000

    Added test for initialising RadioVisServer

---
+++ test/radiovis-server.js
@@ -0,0 +1,36 @@
+'use strict'
+
+var assert = require('assert')
+var RadioVisServer = require('../lib/radiovis-server')
+var RadioVisService = require('../lib/radiovis-service')
+
+describe('RadioVisServer', function () {
+  describe('initialising', function () {
+    it('should fall back to default settings', function () {
+      var serviceData = {
+        'london': {
+          'name': 'BBC Radio London',
+          'bearers': ['dab:ce1.c186.cc31.0']
+        }
+      }
+
+      var server = new RadioVisServer({}, serviceData)
+      assert.equal(server.stompPort, 61613)
+      assert.equal(server.stompHost, '0.0.0.0')
+      assert.equal(server.adminPort, 7443)
+      assert.equal(server.adminHost, '127.0.0.1')
+      assert.equal(typeof server.services, 'object')
+      assert(server.services['london'] instanceof RadioVisService)
+      assert.deepEqual(server.connections, [])
+      assert.deepEqual(server.topicMap, {
+        'dab/ce1/c186/cc31/0': {
+          serviceId: 'london',
+          subscribers: {
+            image: [],
+            text: []
+          }
+        }
+      })
+    })
+  })
+})
