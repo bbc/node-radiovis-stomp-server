87b9034ff8e43975d9872c3dcc31c3398055e28b
Nicholas Humfrey <njh@aelius.com>
2017-04-04 09:19:57 +0100

    Wrapped up StompFrame tests in an outer describe()

--- test/stomp/frame.js
+++ test/stomp/frame.js
@@ -3,106 +3,110 @@
 var assert = require('assert')
 var StompFrame = require('../../lib/stomp/frame')
 
-describe('CONNECT frame', function () {
-  it('should parse the frame correctly', function () {
-    var buffer = new Buffer('CONNECT\x0A\x0A\x00')
-    var frame = StompFrame.parse(buffer)
-    assert.equal(frame.command, 'CONNECT')
-    assert.deepEqual(frame.headers, {})
-    assert.equal(frame.body, '')
+describe('StompFrame', function () {
+
+  describe('CONNECT frame', function () {
+    it('should parse the frame correctly', function () {
+      var buffer = new Buffer('CONNECT\x0A\x0A\x00')
+      var frame = StompFrame.parse(buffer)
+      assert.equal(frame.command, 'CONNECT')
+      assert.deepEqual(frame.headers, {})
+      assert.equal(frame.body, '')
+    })
+
+    it('should parse the frame with CR LF correctly', function () {
+      var buffer = new Buffer('CONNECT\x0D\x0A\x0D\x0A\x00')
+      var frame = StompFrame.parse(buffer)
+      assert.equal(frame.command, 'CONNECT')
+      assert.deepEqual(frame.headers, {})
+      assert.equal(frame.body, '')
+    })
+
+    it('should be tolerant of whitespace before the frame command', function () {
+      var buffer = new Buffer('  CONNECT\x0D\x0A\x0D\x0A\x00')
+      var frame = StompFrame.parse(buffer)
+      assert.equal(frame.command, 'CONNECT')
+      assert.deepEqual(frame.headers, {})
+      assert.equal(frame.body, '')
+    })
+
+    it('should generate a frame correctly', function () {
+      var frame = new StompFrame('CONNECT')
+      assert.equal(frame.toBuffer(), 'CONNECT\x0A\x0A\x00')
+    })
   })
 
-  it('should parse the frame with CR LF correctly', function () {
-    var buffer = new Buffer('CONNECT\x0D\x0A\x0D\x0A\x00')
-    var frame = StompFrame.parse(buffer)
-    assert.equal(frame.command, 'CONNECT')
-    assert.deepEqual(frame.headers, {})
-    assert.equal(frame.body, '')
+  describe('SUBSCRIBE frame', function () {
+    it('should parse the frame correctly', function () {
+      var buffer = new Buffer('SUBSCRIBE\x0Adestination:/topic/fm/ce1/c201/09880/text\x0Aack:auto\x0A\x0A\x00')
+      var frame = StompFrame.parse(buffer)
+      assert.equal(frame.command, 'SUBSCRIBE')
+      assert.deepEqual(frame.headers, {'destination': '/topic/fm/ce1/c201/09880/text', 'ack': 'auto'})
+      assert.equal(frame.body, '')
+    })
+
+    it('should parse the frame with CR LF correctly', function () {
+      var buffer = new Buffer('SUBSCRIBE\x0D\x0Adestination:/topic/fm/ce1/c201/09880/text\x0D\x0Aack:auto\x0D\x0A\x0D\x0A\x00')
+      var frame = StompFrame.parse(buffer)
+      assert.equal(frame.command, 'SUBSCRIBE')
+      assert.deepEqual(frame.headers, {'destination': '/topic/fm/ce1/c201/09880/text', 'ack': 'auto'})
+      assert.equal(frame.body, '')
+    })
+
+    it('should parse a header with a space after the colon', function () {
+      var buffer = new Buffer('SUBSCRIBE\x0Adestination: /topic/fm/ce1/c201/09880/text\x0Aack : auto\x0A\x0A\x00')
+      var frame = StompFrame.parse(buffer)
+      assert.equal(frame.command, 'SUBSCRIBE')
+      assert.deepEqual(frame.headers, {'destination': '/topic/fm/ce1/c201/09880/text', 'ack': 'auto'})
+      assert.equal(frame.body, '')
+    })
+
+    it('should generate a frame correctly', function () {
+      var frame = new StompFrame('SUBSCRIBE', {'destination': '/topic/fm/ce1/c201/09880/text', 'ack': 'auto'})
+      assert.equal(frame.toBuffer(), 'SUBSCRIBE\x0Adestination:/topic/fm/ce1/c201/09880/text\x0Aack:auto\x0A\x0A\x00')
+    })
   })
 
-  it('should be tolerant of whitespace before the frame command', function () {
-    var buffer = new Buffer('  CONNECT\x0D\x0A\x0D\x0A\x00')
-    var frame = StompFrame.parse(buffer)
-    assert.equal(frame.command, 'CONNECT')
-    assert.deepEqual(frame.headers, {})
-    assert.equal(frame.body, '')
+  describe('MESSAGE frame', function () {
+    it('should parse the frame correctly', function () {
+      var buffer = new Buffer('MESSAGE\x0Adestination:/topic/fm/ce1/c201/09880/text\x0A\x0AHello World\x00')
+      var frame = StompFrame.parse(buffer)
+      assert.equal(frame.command, 'MESSAGE')
+      assert.deepEqual(frame.headers, {'destination': '/topic/fm/ce1/c201/09880/text'})
+      assert.equal(frame.body, 'Hello World')
+    })
+
+    it('should parse the frame with CR LF correctly', function () {
+      var buffer = new Buffer('MESSAGE\x0D\x0Adestination:/topic/fm/ce1/c201/09880/text\x0D\x0A\x0D\x0AHello World\x00')
+      var frame = StompFrame.parse(buffer)
+      assert.equal(frame.command, 'MESSAGE')
+      assert.deepEqual(frame.headers, {'destination': '/topic/fm/ce1/c201/09880/text'})
+      assert.equal(frame.body, 'Hello World')
+    })
+
+    it('should generate a frame correctly', function () {
+      var frame = new StompFrame(
+        'MESSAGE',
+        {
+          'destination': '/topic/fm/ce1/c201/09880/image',
+          'link': 'http://www.bbc.co.uk/'
+        },
+        'SHOW http://static.bbci.co.uk/radiovis/1.1.0/logos/bbc_radio_one.png'
+      )
+      assert.equal(
+        frame.toBuffer(),
+        'MESSAGE\x0Adestination:/topic/fm/ce1/c201/09880/image\x0Alink:http://www.bbc.co.uk/\x0A\x0A' +
+        'SHOW http://static.bbci.co.uk/radiovis/1.1.0/logos/bbc_radio_one.png\x00'
+      )
+    })
   })
 
-  it('should generate a frame correctly', function () {
-    var frame = new StompFrame('CONNECT')
-    assert.equal(frame.toBuffer(), 'CONNECT\x0A\x0A\x00')
-  })
-})
-
-describe('SUBSCRIBE frame', function () {
-  it('should parse the frame correctly', function () {
-    var buffer = new Buffer('SUBSCRIBE\x0Adestination:/topic/fm/ce1/c201/09880/text\x0Aack:auto\x0A\x0A\x00')
-    var frame = StompFrame.parse(buffer)
-    assert.equal(frame.command, 'SUBSCRIBE')
-    assert.deepEqual(frame.headers, {'destination': '/topic/fm/ce1/c201/09880/text', 'ack': 'auto'})
-    assert.equal(frame.body, '')
+  describe('Invalid frame', function () {
+    it('parsing the frame should return undefined', function () {
+      var buffer = new Buffer('Foo Bar\x00')
+      var frame = StompFrame.parse(buffer)
+      assert.equal(frame, undefined)
+    })
   })
 
-  it('should parse the frame with CR LF correctly', function () {
-    var buffer = new Buffer('SUBSCRIBE\x0D\x0Adestination:/topic/fm/ce1/c201/09880/text\x0D\x0Aack:auto\x0D\x0A\x0D\x0A\x00')
-    var frame = StompFrame.parse(buffer)
-    assert.equal(frame.command, 'SUBSCRIBE')
-    assert.deepEqual(frame.headers, {'destination': '/topic/fm/ce1/c201/09880/text', 'ack': 'auto'})
-    assert.equal(frame.body, '')
-  })
-
-  it('should parse a header with a space after the colon', function () {
-    var buffer = new Buffer('SUBSCRIBE\x0Adestination: /topic/fm/ce1/c201/09880/text\x0Aack : auto\x0A\x0A\x00')
-    var frame = StompFrame.parse(buffer)
-    assert.equal(frame.command, 'SUBSCRIBE')
-    assert.deepEqual(frame.headers, {'destination': '/topic/fm/ce1/c201/09880/text', 'ack': 'auto'})
-    assert.equal(frame.body, '')
-  })
-
-  it('should generate a frame correctly', function () {
-    var frame = new StompFrame('SUBSCRIBE', {'destination': '/topic/fm/ce1/c201/09880/text', 'ack': 'auto'})
-    assert.equal(frame.toBuffer(), 'SUBSCRIBE\x0Adestination:/topic/fm/ce1/c201/09880/text\x0Aack:auto\x0A\x0A\x00')
-  })
-})
-
-describe('MESSAGE frame', function () {
-  it('should parse the frame correctly', function () {
-    var buffer = new Buffer('MESSAGE\x0Adestination:/topic/fm/ce1/c201/09880/text\x0A\x0AHello World\x00')
-    var frame = StompFrame.parse(buffer)
-    assert.equal(frame.command, 'MESSAGE')
-    assert.deepEqual(frame.headers, {'destination': '/topic/fm/ce1/c201/09880/text'})
-    assert.equal(frame.body, 'Hello World')
-  })
-
-  it('should parse the frame with CR LF correctly', function () {
-    var buffer = new Buffer('MESSAGE\x0D\x0Adestination:/topic/fm/ce1/c201/09880/text\x0D\x0A\x0D\x0AHello World\x00')
-    var frame = StompFrame.parse(buffer)
-    assert.equal(frame.command, 'MESSAGE')
-    assert.deepEqual(frame.headers, {'destination': '/topic/fm/ce1/c201/09880/text'})
-    assert.equal(frame.body, 'Hello World')
-  })
-
-  it('should generate a frame correctly', function () {
-    var frame = new StompFrame(
-      'MESSAGE',
-      {
-        'destination': '/topic/fm/ce1/c201/09880/image',
-        'link': 'http://www.bbc.co.uk/'
-      },
-      'SHOW http://static.bbci.co.uk/radiovis/1.1.0/logos/bbc_radio_one.png'
-    )
-    assert.equal(
-      frame.toBuffer(),
-      'MESSAGE\x0Adestination:/topic/fm/ce1/c201/09880/image\x0Alink:http://www.bbc.co.uk/\x0A\x0A' +
-      'SHOW http://static.bbci.co.uk/radiovis/1.1.0/logos/bbc_radio_one.png\x00'
-    )
-  })
-})
-
-describe('Invalid frame', function () {
-  it('parsing the frame should return undefined', function () {
-    var buffer = new Buffer('Foo Bar\x00')
-    var frame = StompFrame.parse(buffer)
-    assert.equal(frame, undefined)
-  })
 })
