2d8b73f6f66f4d2ecd089e36158ba1f244b61505
Nicholas Humfrey <njh@aelius.com>
2017-04-05 23:36:55 +0100

    Wrote some tests for StompConnection.prototype.handleData

---
+++ test/stomp/connection.js
@@ -0,0 +1,106 @@
+'use strict'
+
+var assert = require('assert')
+var sinon = require('sinon')
+var net = require('net')
+var StompConnection = require('../../lib/stomp/connection')
+var StompFrame = require('../../lib/stomp/frame')
+
+var server
+var socket
+beforeEach(function () {
+  server = {}
+  socket = new net.Socket()
+})
+
+describe('StompConnection', function () {
+  it('should create a new connection successfully', function () {
+    var connection = new StompConnection(socket, server)
+    assert(connection.connected)
+  })
+
+  describe('receiving data', function () {
+    var stubFrame
+    var stubError
+    var connection
+    beforeEach(function () {
+      connection = new StompConnection(socket, server)
+      stubFrame = sinon.stub(connection, 'handleFrame')
+      stubError = sinon.stub(connection, 'sendError')
+    })
+
+    it('should not call handleFrame an incomplete frame', function () {
+      connection.handleData(
+        new Buffer('CONNECT')
+      )
+      assert(stubFrame.notCalled)
+    })
+
+    it('should call handleFrame once for a single complete frame', function () {
+      connection.handleData(
+        new Buffer('CONNECT\n\n\0')
+      )
+      assert.equal(stubFrame.callCount, 1)
+      assert.equal(stubError.callCount, 0)
+
+      var connectFrame = new StompFrame('CONNECT')
+      assert.deepEqual(stubFrame.args[0][0], connectFrame)
+    })
+
+    it('should call handleFrame twice for two frames in a single buffer', function () {
+      connection.handleData(
+        new Buffer('CONNECT\n\n\0SUBSCRIBE\ndestination:foo\n\n\0')
+      )
+      assert.equal(stubFrame.callCount, 2)
+      assert.equal(stubError.callCount, 0)
+      assert.equal(stubFrame.args[0][0].command, 'CONNECT')
+      assert.equal(stubFrame.args[1][0].command, 'SUBSCRIBE')
+    })
+
+    it('should call handleFrame once for a frame split over two buffers', function () {
+      connection.handleData(
+        new Buffer('SUBSCRIBE\n')
+      )
+      connection.handleData(
+        new Buffer('destination:foo\n\n\0')
+      )
+
+      assert.equal(stubFrame.callCount, 1)
+      assert.equal(stubError.callCount, 0)
+      assert.equal(stubFrame.args[0][0].command, 'SUBSCRIBE')
+      assert.equal(stubFrame.args[0][0].headers.destination, 'foo')
+    })
+
+    it('should call handleFrame two for 2 frame split over two buffers', function () {
+      connection.handleData(
+        new Buffer('CONNECT\n\n\0SUBSCRIBE\n')
+      )
+      connection.handleData(
+        new Buffer('destination:foo\n\n\0')
+      )
+
+      assert.equal(stubFrame.callCount, 2)
+      assert.equal(stubError.callCount, 0)
+      assert.equal(stubFrame.args[0][0].command, 'CONNECT')
+      assert.equal(stubFrame.args[1][0].command, 'SUBSCRIBE')
+    })
+
+    it('should send an error if too much data is recieved', function () {
+      connection.handleData(
+        new Buffer(1024 * 1024)
+      )
+
+      assert.equal(stubFrame.callCount, 0)
+      assert.equal(stubError.args[0][0], 'Client sent too much data')
+    })
+
+    it('should send an error if a bad frame is recieved', function () {
+      connection.handleData(
+        new Buffer('foobar\0')
+      )
+
+      assert.equal(stubFrame.callCount, 0)
+      assert.equal(stubError.args[0][0], 'Unable to parse STOMP frame')
+    })
+  })
+})
